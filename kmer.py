13de6ad99b02f075d257607b8819ed303d2b4e28
ahoarfrost <adrienne.l.hoarfrost@gmail.com>
2015-08-14 20:21:47 -0400

    counting kmers

--- kmer.py
+++ kmer.py
@@ -14,10 +14,31 @@ for line in open ("aa.txt"):
     line = line.strip()
     aalist.append(line)
 
-start = aalist[0]
-print start
+#start = aalist[0]
+#print start
+
+aadict = {}
 
 #kmers of 4
 k=5
-for j in range(0,len(start) - k + 1):
-    print j, start[j:j+k]
+for read in aalist:
+    for j in range(0,len(read) - k + 1):
+        kmer = read[j:j+k]
+        aadict[kmer] = aadict.get(kmer,0)+1
+        print j, kmer, aadict[kmer]
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
